80fd7e17880206e3d8ee55a9d5d3f608917afbad
stefanlazarevic <stefanlaza97@gmail.com>
2023-06-12 15:07:14 -0700

    add-super-computer-player

--- tic-tac-toe-ai/lib/super_computer_player.rb
+++ tic-tac-toe-ai/lib/super_computer_player.rb
@@ -1,14 +1,28 @@
 require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
-  def move(game, mark)
-  end
+    def move(game, mark)
+        node = TicTacToeNode.new(game.board, mark)
+        node.children.each do |child|
+            if child.winning_node?(mark)
+                return child.prev_move_pos
+            end
+        end
+
+        node.children.each do |child|
+            unless child.losing_node?(mark)
+                return child.prev_move_pos
+            end
+        end
+        
+        raise "you are a awesome person, have a great day!"
+    end
 end
 
 if $PROGRAM_NAME == __FILE__
-  puts "Play the brilliant computer!"
-  cp1 = HumanPlayer.new()
-  cp2S = SuperComputerPlayer.new
+    puts "Play the brilliant computer!"
+    cp1 = HumanPlayer.new()
+    cp2 = SuperComputerPlayer.new
 
-  TicTacToe.new(hp, cp).run
+    TicTacToe.new(cp1, cp2).run
 end
